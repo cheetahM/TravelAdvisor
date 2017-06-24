827e7766d10d423f59d2d614f5ca0a06a1dbd974
Chetan Meshram <chmeshra@visa.com>
2017-06-24 08:13:40 -0700

    adding assets

---
+++ assets/js/events.js
@@ -0,0 +1,216 @@
+
+$("#findEvent").on("click", function(event){
+	event.preventDefault();
+	console.log("hello.. selected flight: ", selectedFlight);		
+	// var location = $("#destination-input").val().trim();
+	// var startDate = $("#arrival-input").val().trim();
+	// var departDate = $("#departure-input").val().trim();
+
+	// Now will get location start and depart dates from flight info. Placeholders declared below:
+
+	var location = selectedFlight.destination;
+	var startDate = 20170701;
+	var departDate = 20170710;
+
+	var eventList = [];
+
+	var dateRange = (startDate + "-" + departDate);
+	var interests = $("#interests").val().trim();
+	
+	var eventSearchURL = "http://crossorigin.me/http://api.eventful.com/json/events/search?app_key=wHxG23mXN4PJV7cR&page_size=5&page_number=1&location=" + location + "&date=" + dateRange + "&keywords=" + interests;
+	
+	console.log(eventSearchURL);
+	showEventResults();
+
+
+// ------------- Ajax Search call when Eventful is working ----------------------
+
+	$.ajax({
+	    url: eventSearchURL,
+	    method: "GET",
+	    dataType: "json"
+	}).done(function(response) {
+
+		for (var i = 0; i<5; i++){
+			
+			var a = $("<a></a><br>").addClass("event-button-wrapper");
+
+			var eventTitle = $("<span></span><br>").addClass("buy-button-title").attr("data-eventfulid", response.events.event[i].id).attr("value",response.events.event[i].title).text(response.events.event[i].title);
+			a.append(eventTitle);
+
+			var eventStartResult = $("<span></span>").addClass("buy-button-start-time").text("Start Time: " + response.events.event[i].start_time);
+			a.append(eventStartResult);
+
+			var eventEndResult = $("<span></span><br>").addClass("buy-button-end-time").text(" End Time: " + response.events.event[i].stop_time);
+			a.append(eventEndResult);
+
+			var eventVenueResult = $("<span></span><br>").addClass("buy-button-venue").text("Venue: " + response.events.event[i].venue_name);
+			a.append(eventVenueResult);
+
+			var eventPriceButton = $("<span></span><br>").attr("data-eventfulid", response.events.event[i].id).attr("id", "price-button-id-" + i).addClass("event-detail-button").text("Click for Pricing");
+			a.append(eventPriceButton);
+
+
+			$("#event-results").append(a);
+		}
+			$(".buy-button-title").on("click", function(event){
+				y = $(this).attr("data-eventfulid");
+				z = selectEventPrice(y);
+				console.log(z);
+				var x = $(this).text();
+				console.log(x);
+				$("#itinerary-results").text(x + z);
+				showItineraryResults();
+                $('html,body').animate({
+                    scrollTop: $('#itinerary').offset().top},'slow');
+			});
+	
+	})
+	
+// ----------------------- End Get Call -----------------------------------------------------------
+
+
+
+// ------------ Hard coded values for testing with Eventful 522 Errors -------------------
+
+			// var a = $("<a></a>").addClass("event-button-wrapper");
+
+			// var eventTitle = $("<span></span>").addClass("buy-button-title").text("Title");
+			// a.append(eventTitle);
+
+			// var eventStartResult = $("<span></span>").addClass("buy-button-start-time").text("Start Time: 2pm");
+			// a.append(eventStartResult);
+
+			// var eventEndResult = $("<span></span>").addClass("buy-button-end-time").text("End Time: 5pm");
+			// a.append(eventEndResult);
+
+			// var eventVenueResult = $("<span></span>").addClass("buy-button-venue").text("Venue: The Double Deuce");
+			// a.append(eventVenueResult);
+
+			// var eventPriceButton = $("<button>").attr("data-eventfulid", "E0-001-099584198-2").attr("id", "price-button-id-" + 0).addClass("event-detail-button").text("Click for Pricing");
+			// a.append(eventPriceButton);
+
+			// $("#event-results").append(a);
+
+// --------------------- End hard coded values -------------------------------------
+
+
+});
+
+function showEventResults(){
+	var x = document.getElementById('eventTable');
+	if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+function showItineraryResults(){
+	var x = document.getElementById('itineraryTable');
+	if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'block';
+    }
+}
+
+
+function displayEventPrice() {
+
+	var eventfulID = $(this).attr("data-eventfulid");
+	var buttonID = $(this).attr("id");
+	console.log(buttonID);
+	var eventGetURL = "http://crossorigin.me/http://api.eventful.com/json/events/get?app_key=wHxG23mXN4PJV7cR&id=" + eventfulID;
+	console.log(eventGetURL);
+
+
+
+// ------------- Ajax Get call when Eventful is working ----------------------
+
+	$.ajax({
+		url: eventGetURL,
+		method: "GET",
+		dataType: "json"
+
+	}).done(function(response) {
+
+		console.log(response.free);
+		console.log(response.price);
+
+		if (response.free === 1){
+
+		$("#" + buttonID).text("Free!");
+
+		} else if (response.price === null){
+
+// Just going to say it's free although may be a missing value -- could do "Unknown" as well
+
+			$("#" + buttonID).text("Free!");
+
+		} else {
+
+			$("#" + buttonID).text(response.price);
+
+		}
+
+						
+	});
+
+
+
+
+
+// ----------------------- End Get Call -----------------------------------------------------------
+
+
+// ------------ Hard coded values for testing with Eventful 522 Errors -------------------
+
+	// $("#" + buttonID).text("$27");
+
+// --------------------- End hard coded values -------------------------------------
+
+		
+};
+
+function selectEventPrice(a) {
+
+	var eventfulID = a;
+	var eventGetURL = "http://crossorigin.me/http://api.eventful.com/json/events/get?app_key=wHxG23mXN4PJV7cR&id=" + eventfulID;
+	console.log(eventGetURL);
+
+
+
+// ------------- Ajax Get call when Eventful is working ----------------------
+
+	$.ajax({
+		url: eventGetURL,
+		method: "GET",
+		dataType: "json"
+
+	}).done(function(response) {
+
+		console.log(response.free);
+		console.log(response.price);
+
+		if (response.free === 1){
+
+			$("#itinerary-results").append(" Free");
+
+		} else if (response.price === null){
+
+// Just going to say it's free although may be a missing value -- could do "Unknown" as well
+
+			$("#itinerary-results").append(" Free!");
+
+		} else {
+
+			$("#itinerary-results").append( response.price);
+
+		}
+						
+	});
+
+	};	
+
+$(document).on("click", ".event-detail-button", displayEventPrice);
---
+++ assets/js/flights.js
@@ -0,0 +1,137 @@
+var flightData = {
+  "request": {
+    "slice": [
+      {
+        "origin": "SFO",
+        "destination": "NYC",
+        "date": "2017-06-27"
+      },
+      {
+        "origin": "NYC",
+        "destination": "SFO",
+        "date": "2017-06-28"
+      }
+    ],
+    "passengers": {
+      "adultCount": 1,
+      "infantInLapCount": 0,
+      "infantInSeatCount": 0,
+      "childCount": 0,
+      "seniorCount": 0
+    },
+    "solutions": 10,
+    "refundable": false
+  }
+};
+
+var flightInfo = [];
+var selectedFlight ={};
+var jsonObject = {};
+var airline;
+var price;
+var origin = $('#fOrigin').val();
+var destination = $('#fDestination').val();
+var departingDate = $('#departingDate').val();
+var returningDate = $('#returningDate').val();
+
+var apikey = "AIzaSyBf_sxIn1lzHh9N4IkjVZK5_rIqkgak7mw";
+var endPoint = "https://www.googleapis.com/qpxExpress/v1/trips/search?key="+apikey;
+
+$( "#searchFlights" ).submit(function(event) {
+  // Stop form from submitting normally
+  event.preventDefault();
+  flightData.request.slice[0].origin = $('#fOrigin').val();
+  flightData.request.slice[0].destination = $('#fDestination').val();
+  flightData.request.slice[1].origin = $('#fDestination').val();
+  flightData.request.slice[1].destination = $('#fOrigin').val();
+  flightData.request.passengers.adultCount = $('#ticketCount').val();
+  flightData.request.maxPrice = $('#maxPrice').val();
+  flightData.request.slice[0].date = $('#departingDate').val();
+  flightData.request.slice[1].date = $('#returningDate').val();
+  // console.log(flightData.request.slice[0].destination);
+  
+  $.ajax({
+    type: "POST",
+    //Set up your request URL and API Key.
+    url: "https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyBf_sxIn1lzHh9N4IkjVZK5_rIqkgak7mw",
+    // Set Content-type: application/json
+    contentType: 'application/json',
+    dataType: 'json',
+    // The query we want from Google QPX, This will be the variable we created in the beginning
+    data: JSON.stringify(flightData),
+    success: function(data) {
+        //Once we get the result you can either send it to console or use it anywhere you like.
+        console.log(JSON.stringify("Google flights respons is: ", JSON.stringify(data)));
+        //var content = $( data ).find( "#content" );
+        //$( "#results" ).empty().append( JSON.stringify(data, null, 2));
+        for(i=0; i < data.trips.tripOption.length; i++){
+            airline = data.trips.tripOption[i].slice[0].segment[0].flight.carrier;
+            price = data.trips.tripOption[i].saleTotal;
+            origin = data.trips.tripOption[i].slice[0].segment[0].leg[0].origin;
+            destination = data.trips.tripOption[i].slice[0].segment[0].leg[0].destination;
+            departureTime = data.trips.tripOption[i].slice[0].segment[0].leg[0].departureTime;
+            arrivalTime = data.trips.tripOption[i].slice[0].segment[0].leg[0].arrivalTime;
+            jsonObject = {"airline": airline , "price": price, "origin": origin,
+                "destination": destination, "departureTime": departureTime, "arrivalTime": arrivalTime };
+            flightInfo.push(jsonObject);
+        }
+
+        $.each(flightInfo, function(i, item) {
+            console.log("inside each....");
+            var $tr = $('<tr>').addClass('clickable-row page-scroll');
+            $tr.append(
+                $('<td>').text(item.airline),
+                $('<td>').text(item.price),
+                $('<td>').text(item.origin),
+                $('<td>').text(item.destination),
+                $('<td>').text(item.departureTime),
+                $('<td>').text(item.arrivalTime)
+            ).appendTo('#flightResults');
+        });
+        $('#flightsTable').removeClass('hide');
+        $('#flightsTable').addClass('show');
+        $('html,body').animate({
+            scrollTop: $('#flightsTable').offset().top},'slow');
+            
+    },
+    error: function() {
+        //Error Handling for our request
+        console.log("Access to Google QPX Failed.");
+    }
+ });
+});
+
+$('#flightsTable tr').on("click", function(e) {
+    console.log($(this));
+});
+
+$("input").on("change", function() {
+    this.setAttribute(
+        "data-date",
+        moment(this.value, "YYYY-MM-DD")
+        .format( this.getAttribute("data-date-format") )
+    )
+}).trigger("change");
+
+$('#flightsTable').on('click', '.clickable-row', function(event) {
+  
+                var tableData = $(this).children('td').map(function () {
+                    return $(this).text();
+                }).get();
+                var props = $('thead > tr th');
+                var array = [];
+                props.each(function () { array.push($(this).text().toLowerCase()) });
+                //keys
+                console.log(array);
+                //values
+                console.log(tableData);
+
+
+                for (var i = 0; i < tableData.length; i++) {
+                    selectedFlight[array[i]] = tableData[i];
+                }
+                console.log(selectedFlight);
+                $('#events').addClass('show');
+                $('html,body').animate({
+                    scrollTop: $('#events').offset().top},'slow');
+});
